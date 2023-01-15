f58812c77a9384a949defe3233b934e6554cff71
Erika Londono <erika@MacBook-Air-de-Erika.local>
2023-01-15 10:28:01 -0500

    Agregando archivos

---
+++ script.js
@@ -0,0 +1,34 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input,
+});
+
+h1.innerHTML = 'Patito <br> Feo';
+h1.innerText = 'Patito <br> Feo';
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class', 'rojo');
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
+
+input.value = '456';
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://i.pinimg.com/originals/8b/02/40/8b02409f975c98c89e15bc089e3bd289.gif');
+console.log(img);
+
+pid.innerHTML = "";
+pid.appendChild(img);
